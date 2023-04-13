1768653f0a0d9444826b471b9255a9ab5c2f7545
annanharry <annanharry7@gmail.com>
2023-04-13 13:15:06 -0700

    extra tasks

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,7 +5,21 @@
 /* helpers */
 void print_array(int *array, size_t inc, size_t size);
 
+typedef struct skiplist_s
+{
+				int n;
+				size_t index;
+				struct skiplist_s *next;
+				struct skiplist_s *express;
+} skiplist_t;
+
 /* main functions */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+
+skiplist_t *linear_skip(skiplist_t *list, int value);
 #endif
